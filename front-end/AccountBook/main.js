68626c8a18792a0997f77d30a4ea000daff28301
Soyeon Choe <77609591+soi-ha@users.noreply.github.com>
2022-01-05 01:10:31 +0900

    Delete front-end directory

--- front-end/AccountBook/main.js
+++
@@ -1,8 +0,0 @@
-const toggleBtn = document.querySelector('.headvar .toggleBtn');
-const menu = document.querySelector('.headvar .menu');
-const icon = document.querySelector('.headvar .icon');
-
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    icon.classList.toggle('active');
-});
--- front-end/yeoni/main.js
+++
@@ -1,8 +0,0 @@
-const toggleBtn = document.querySelector('.y_toggleBtn');
-const menu = document.querySelector('.y_menu');
-const icon = document.querySelector('.y_icon');
-
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    icon.classList.toggle('active');
-});
