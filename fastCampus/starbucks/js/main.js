1502334b2b35a32a9a9fe2e0ce8d77d0d9ffb211
soi-ha <soy2302ten@gmail.com>
2022-04-05 17:04:36 +0900

    footer done

--- fastCampus/starbucks/js/main.js
+++ fastCampus/starbucks/js/main.js
@@ -144,3 +144,6 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 });
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear(); // 올해를 계산해줌
