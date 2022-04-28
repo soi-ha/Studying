3cb01906a5c1c2e21266d94c5c71f41594d28dd8
soi-ha <soy2302ten@gmail.com>
2022-04-28 17:31:48 +0900

    JS시작하기 - 비교,논리 연산자까지

---
+++ fastCampus/js-practice/js-test/main.js
@@ -0,0 +1,32 @@
+// 데이터 타입 확인
+// import getType from './getType'
+
+// console.log(typeof 'Hello World!')
+// console.log(typeof 123)
+// console.log(typeof true)
+// console.log(typeof undefined)
+// console.log(typeof null)
+// console.log(typeof {})
+// console.log(typeof [])
+
+// console.log(getType(123))
+// console.log(getType(false))
+// console.log(getType(null))
+// console.log(getType({}))
+// console.log(getType([]))
+
+// 산술 연산자
+
+// console.log(1 + 2)
+// console.log(5 - 7)
+// console.log(3 * 4)
+// console.log(10 / 2)
+// console.log(7 % 5)
+
+// 할당 연산자
+
+let a = 2
+// a = a + 1
+a %= 1
+
+console.log(a)
