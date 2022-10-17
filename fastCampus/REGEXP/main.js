5649071cf68f1ff3e7808d8989bdba5b63a9931b
soi-ha <soy2302ten@gmail.com>
2022-10-17 17:42:17 +0900

    표현(1)

--- fastCampus/REGEXP/main.js
+++ fastCampus/REGEXP/main.js
@@ -4,6 +4,9 @@ thesoha@gmail.com
 https://soi-ha.github.io/
 The quick brown fox jumps over the lazy dog.
 abbcccdddd
+http://localhost:1234
 `
 
-console.log(str.match(/\.$/gim))
+console.log(
+  str.match(/\b\w{2,3}\b/g)
+)
