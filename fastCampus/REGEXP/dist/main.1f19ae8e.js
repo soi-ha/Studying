a49f392ed67419e7d4ec4c305c2bea5fc9aa8c09
soi-ha <soy2302ten@gmail.com>
2022-10-17 18:39:22 +0900

    표현(2)

--- fastCampus/REGEXP/dist/main.1f19ae8e.js
+++ fastCampus/REGEXP/dist/main.1f19ae8e.js
@@ -118,8 +118,8 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"main.js":[function(require,module,exports) {
-var str = "\n010-1234-5678\nthesoha@gmail.com\nhttps://soi-ha.github.io/\nThe quick brown fox jumps over the lazy dog.\nabbcccdddd\nhttp://localhost:1234\n";
-console.log(str.match(/\b\w{2,3}\b/g));
+var str = "\n010-1234-5678\nthesoha@gmail.com\nhttps://soi-ha.github.io/\nThe quick brown fox jumps over the lazy dog.\nabbcccdddd\n";
+console.log(str.match(/(?<=@).{1,}/g));
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -148,7 +148,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60318" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63946" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- fastCampus/REGEXP/main.js
+++ fastCampus/REGEXP/main.js
@@ -4,9 +4,8 @@ thesoha@gmail.com
 https://soi-ha.github.io/
 The quick brown fox jumps over the lazy dog.
 abbcccdddd
-http://localhost:1234
 `
 
 console.log(
-  str.match(/\b\w{2,3}\b/g)
+  str.match(/(?<=@).{1,}/g)
 )
