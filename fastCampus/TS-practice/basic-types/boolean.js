f722537d12e2680259108f22fb81267b4e5cdd04
soi-ha <soy2302ten@gmail.com>
2022-10-24 16:44:00 +0900

    패캠 boolean, number 공부

---
+++ fastCampus/TS-practice/basic-types/boolean.js
@@ -0,0 +1,7 @@
+"use strict";
+let isDone = false;
+isDone = true;
+console.log(typeof isDone); //'boolean'
+let isOk = true;
+console.log(typeof isOk);
+// let isNotOk: boolean = new Boolean(true);
---
+++ fastCampus/TS-practice/basic-types/boolean.ts
@@ -0,0 +1,12 @@
+let isDone: boolean  = false;
+
+isDone = true;
+
+console.log(typeof isDone);  //'boolean'
+
+
+let isOk: Boolean = true;
+console.log(isOk); // true
+console.log(typeof isOk); // 'boolean'
+
+let isNotOk: boolean = new Boolean(true);
