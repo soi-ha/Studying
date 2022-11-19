f7607c0129fd833d0d1f506d2586fc02f198c181
soi-ha <soy2302ten@gmail.com>
2022-11-19 22:53:08 +0900

    interface 사용 후, hello 함수

--- fastCampus/TS-practice/interface/interface1.ts
+++ fastCampus/TS-practice/interface/interface1.ts
@@ -1,8 +1,13 @@
-function hello(person: { name: string; age: number } ): void {
+interface Person1 {
+  name: string; 
+  age: number;
+}
+
+function hello(person:  Person1 ): void {
   console.log(`안녕하세요! ${person.name}입니다.`)
 }
 
-const p1: { name: string; age: number } = {
+const p1:  Person1 = {
   name: 'Mark',
   age: 39,
 };
