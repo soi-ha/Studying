da784598240f1658eef92ac33305a123427c27df
soi-ha <soy2302ten@gmail.com>
2022-11-19 23:40:06 +0900

    interface안에 함수 만들기 세번째 방법 => 오류 발생

--- fastCampus/TS-practice/interface/interface4.ts
+++ fastCampus/TS-practice/interface/interface4.ts
@@ -19,3 +19,11 @@ const p42: Person4 = {
     console.log(`안녕하세요! ${this.name}입니다.`)
   },
 }
+
+// const p43: Person4 = {
+//   name: 'Mark',
+//   age: 39,
+//   hello: () void => {
+//     console.log(`안녕하세요! ${this.name}입니다.`)
+//   },
+// }
