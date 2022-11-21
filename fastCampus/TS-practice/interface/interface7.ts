15a5adeba38a6f8c4adb56face4c88b65389d2f6
soi-ha <soy2302ten@gmail.com>
2022-11-21 17:05:41 +0900

    function interface 오류 발생의 이유 확인하기

--- fastCampus/TS-practice/interface/interface7.ts
+++ fastCampus/TS-practice/interface/interface7.ts
@@ -2,7 +2,7 @@ interface HelloPerson {
   (name: string, age?: number): void;
 }
 
-const helloPerson: HelloPerson = function (name: string, age?: number){
+const helloPerson: HelloPerson = function (name: string, age: number){
   console.log(`안녕하세요! {name} 입니다!`);
 }
 
