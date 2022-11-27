2aa44eb0a988c5d1815150b551353a8e2143edab
soi-ha <soy2302ten@gmail.com>
2022-11-27 15:27:43 +0900

    error 발생하지 않게 수정

--- fastCampus/TS-practice/class/example.ts
+++ fastCampus/TS-practice/class/example.ts
@@ -1,15 +1,17 @@
 class Person {
-  public readonly name: string = "Mark"
-  private readonly country: string = "Korea"
+  public readonly name: string = "Mark";
+  private readonly country: string;
 
-  public constructor(private _name: string, public age: number) {}
-
-  hello() {
-    this.country = "China";
+  public constructor(private _name: string, public age: number) {
+    this.country = "Korea";
   }
+
+  // hello() {
+  //   this.country = "China";
+  // }
 }
 
 const p1 = new Person("Mark",39);
 console.log(p1.name) // get을 하는 함수 getter
-p1.name = "Jin"; // set을 하는 함수 setter
+// p1.name = "Jin"; // set을 하는 함수 setter
 console.log(p1.name) 
