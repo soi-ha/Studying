9db8954c03004946a267b1df76bf34a1a7b55415
soi-ha <soy2302ten@gmail.com>
2022-11-27 15:21:23 +0900

    readonly 사용으로 private가 있어도 값 변경 불가

--- fastCampus/TS-practice/class/example.ts
+++ fastCampus/TS-practice/class/example.ts
@@ -1,7 +1,12 @@
 class Person {
   public readonly name: string = "Mark"
+  private readonly country: string = "Korea"
 
   public constructor(private _name: string, public age: number) {}
+
+  hello() {
+    this.country = "China";
+  }
 }
 
 const p1 = new Person("Mark",39);
