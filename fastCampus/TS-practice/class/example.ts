f62c53df57c26c790130a27b9e2ad506e0a7014a
soi-ha <soy2302ten@gmail.com>
2022-11-27 15:19:41 +0900

    readonly로 인해 밖에서 name 변경 불가 - error

--- fastCampus/TS-practice/class/example.ts
+++ fastCampus/TS-practice/class/example.ts
@@ -1,16 +1,10 @@
 class Person {
+  public readonly name: string = "Mark"
+
   public constructor(private _name: string, public age: number) {}
-
-  get name() {
-    return this._name + " Lee";
-  }
-
-  set name(n: string) {
-    this._name = n;
-  }
 }
 
-const p1 = new Person("mark",39);
+const p1 = new Person("Mark",39);
 console.log(p1.name) // get을 하는 함수 getter
 p1.name = "Jin"; // set을 하는 함수 setter
 console.log(p1.name) 
