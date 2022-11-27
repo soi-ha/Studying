6d52f88b67d036abb238e7d37ecf99e3e7ed1e5f
soi-ha <soy2302ten@gmail.com>
2022-11-27 14:09:54 +0900

    두가지 상황으로 사용하고 싶을 때

--- fastCampus/TS-practice/class/example.ts
+++ fastCampus/TS-practice/class/example.ts
@@ -1,8 +1,17 @@
 class Person {
-  name: string;
+  name: string = "mark";
   age: number;
+
+  constructor(age?: number) {
+    if (age === undefined) {
+      this.age = 20;
+    } else {
+      this.age = age;
+    }
+  }
 }
 
-const p1 = new Person("Mark");
+const p1 = new Person(39);
+const p2 = new Person();
 
 console.log(p1)
