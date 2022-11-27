8cb08e03c4b05ee09fbba69be352e6b54699495e
soi-ha <soy2302ten@gmail.com>
2022-11-27 15:15:44 +0900

    getter, setter

--- fastCampus/TS-practice/class/example.js
+++ fastCampus/TS-practice/class/example.js
@@ -1,9 +1,17 @@
 "use strict";
 class Person {
-    constructor(name, age) {
-        this.name = name;
+    constructor(_name, age) {
+        this._name = _name;
         this.age = age;
+    }
+    get name() {
+        return this._name + " Lee";
+    }
+    set name(n) {
+        this._name = n;
     }
 }
 const p1 = new Person("mark", 39);
-console.log(p1);
+console.log(p1.name); // get을 하는 함수 getter
+p1.name = "Jin"; // set을 하는 함수 setter
+console.log(p1.name);
--- fastCampus/TS-practice/class/example.ts
+++ fastCampus/TS-practice/class/example.ts
@@ -1,7 +1,16 @@
 class Person {
-  public constructor(public name: string, public age: number) {}
+  public constructor(private _name: string, public age: number) {}
 
+  get name() {
+    return this._name + " Lee";
+  }
+
+  set name(n: string) {
+    this._name = n;
+  }
 }
 
 const p1 = new Person("mark",39);
-console.log(p1)
+console.log(p1.name) // get을 하는 함수 getter
+p1.name = "Jin"; // set을 하는 함수 setter
+console.log(p1.name) 
