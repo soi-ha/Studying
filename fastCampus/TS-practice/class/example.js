b1722614bad5dbb4d42c31f5c7b5b597d8f58ca2
soi-ha <soy2302ten@gmail.com>
2022-11-27 13:58:16 +0900

    quick start class

---
+++ fastCampus/TS-practice/class/example.js
@@ -0,0 +1,8 @@
+"use strict";
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+}
+const p1 = new Person("Mark");
+console.log(p1);
---
+++ fastCampus/TS-practice/class/example.ts
@@ -0,0 +1,11 @@
+class Person {
+  name;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+const p1 = new Person("Mark");
+
+console.log(p1)
