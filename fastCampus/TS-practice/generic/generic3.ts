ba0c4aa4e479b779c781da5ba7b94786d5efbb77
soi-ha <soy2302ten@gmail.com>
2022-12-11 17:17:39 +0900

    generic tuple

--- fastCampus/TS-practice/generic/generic3.ts
+++ fastCampus/TS-practice/generic/generic3.ts
@@ -4,3 +4,10 @@ function helloArray<T>(message: T[]): T {
 
 helloArray(['Hello','Array']); // "Hello"
 helloArray(['Hello', 5]); // string이거나 number?
+
+function helloTuple<T, K>(message: [T, K]): T {
+  return message[0];
+}
+
+helloTuple(['Hello','Array']); // string
+helloTuple(['Hello',5]); // 0번째 index는 string
