e517b0381c6afcc36f570b27a95714f82b666d8f
soi-ha <soy2302ten@gmail.com>
2022-12-11 15:57:16 +0900

    any 타입 사용 -> 예상치 못한 결과

--- fastCampus/TS-practice/generic/generic1.ts
+++ fastCampus/TS-practice/generic/generic1.ts
@@ -5,3 +5,12 @@ function helloString(message: string): string {
 function helloNumber(message: number): number {
   return message
 }
+
+// 더 많이 반복되는 함수들
+
+function hello(message: any): any {
+  return message
+}
+
+console.log(hello("Mark"));
+console.log(hello(19));
