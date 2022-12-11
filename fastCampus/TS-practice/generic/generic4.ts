7a4a5050d94f8f762e38455280b566be0b35e224
soi-ha <soy2302ten@gmail.com>
2022-12-11 17:24:11 +0900

    generic function: interface

--- fastCampus/TS-practice/generic/generic4.ts
+++ fastCampus/TS-practice/generic/generic4.ts
@@ -2,5 +2,15 @@
 type HelloFunctionGeneric1 = <T>(message: T) => T;
 
 const helloFunction1: HelloFunctionGeneric1 = <T>(message: T) => {
-  return message
+  return message;
+};
+
+// interface
+
+interface HelloFunctionGeneric2 {
+  <T>(message: T): T;
+}
+
+const helloFunction2: HelloFunctionGeneric2 = <T>(message: T) => {
+  return message;
 };
