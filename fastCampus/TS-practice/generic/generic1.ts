1dde1caf9cbd67cad75dd9edc0160b1673086d1c
soi-ha <soy2302ten@gmail.com>
2022-12-11 16:10:52 +0900

    Generic 사용

--- fastCampus/TS-practice/generic/generic1.ts
+++ fastCampus/TS-practice/generic/generic1.ts
@@ -14,3 +14,15 @@ function hello(message: any): any {
 
 console.log(hello("Mark"));
 console.log(hello(19));
+
+// Generic
+
+function helloGeneric<T>(message: T): T {
+  return message
+}
+
+console.log(helloGeneric("Mark")); // "Mark"
+console.log(helloGeneric("Mark").length); // number로 인식. String.length
+console.log(helloGeneric(19)); // 19
+console.log(helloGeneric(19).length); // number로 인식했기 때문에 length 사용 불가
+console.log(helloGeneric(true)); // true
