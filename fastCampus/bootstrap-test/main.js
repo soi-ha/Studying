5d9638a7268b14beb6a48c5497eeb9d7b635b5e3
soi-ha <soy2302ten@gmail.com>
2023-01-09 00:01:26 +0900

    npm 관리 및 bootstrap 색상 커스터마이징

--- fastCampus/bootstrap-test/main.js
+++ fastCampus/bootstrap-test/main.js
@@ -1,2 +1,1 @@
-// const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-// const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle'
