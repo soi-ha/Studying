d2ed8bfbb94bbc4ab81b60bb285f9b7fccdf9e44
soi-ha <soy2302ten@gmail.com>
2023-01-15 18:32:54 +0900

    plugins

--- fastCampus/webpack-template-basic/webpack.config.js
+++ fastCampus/webpack-template-basic/webpack.config.js
@@ -1,5 +1,6 @@
 // import
 const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
 
 // export
 module.exports = {
@@ -8,5 +9,12 @@ module.exports = {
     // path: path.resolve(__dirname, 'dist'),
     // filename: 'main.js',
     clean: true
-  }
-}
+  },
+
+  // 번들링 후 결과물의 처리 방식 등 다양한 플러그인들을 설정
+  plugins: [
+    new HtmlPlugin({
+      template: './index.html'
+    })
+  ]
+} 
