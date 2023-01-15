df001c0704d61fad42d31ad6480a4bb89c8b9c98
soi-ha <soy2302ten@gmail.com>
2023-01-15 18:08:52 +0900

    entry, output

---
+++ fastCampus/webpack-template-basic/webpack.config.js
@@ -0,0 +1,12 @@
+// import
+const path = require('path')
+
+// export
+module.exports = {
+  entry: './js/main.js',
+  output: {
+    // path: path.resolve(__dirname, 'dist'),
+    // filename: 'main.js',
+    clean: true
+  }
+}
