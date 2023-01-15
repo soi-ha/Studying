dbfb19b95bef70a4a7dc026239f4f694dca5d507
soi-ha <soy2302ten@gmail.com>
2023-01-16 00:05:52 +0900

    scss

--- fastCampus/webpack-template-basic/js/main.js
+++ fastCampus/webpack-template-basic/js/main.js
@@ -1,3 +1,3 @@
-import '../css/main.css'
+import '../scss/main.scss'
 
 console.log("Webpack!!")
--- fastCampus/webpack-template-basic/webpack.config.js
+++ fastCampus/webpack-template-basic/webpack.config.js
@@ -15,10 +15,11 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.s?css$/,
         use: [
           'style-loader',
-          'css-loader'
+          'css-loader',
+          'sass-loader'
         ]
       }
     ]
