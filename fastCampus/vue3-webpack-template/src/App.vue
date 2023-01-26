6052628a5e7cd60368681c9b409f7d8e3ba46d4b
soi-ha <soy2302ten@gmail.com>
2023-01-26 17:01:46 +0900

    Vue3 webpack template

---
+++ fastCampus/vue3-webpack-template/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ message }}</h1>
+  <HelloWorld />
+</template>
+
+<script>
+import HelloWorld from '~/components/HelloWorld'
+
+export default {
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      message: "Hello Vue!!"
+    }
+  }
+}
+</script>
