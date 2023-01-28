9e8a369b6582541eb6518e35da89cf015e8d855a
soi-ha <soy2302ten@gmail.com>
2023-01-28 19:01:06 +0900

    Vue 문법: Computed

--- fastCampus/vue3-start/src/App.vue
+++ fastCampus/vue3-start/src/App.vue
@@ -1,34 +1,12 @@
 <template>
-  <h1> {{ count }} </h1>
+  <Fruits />
 </template>
 
 <script>
+import Fruits from '~/components/Fruits'
 export default {
-  data() {
-    return {
-      count: 2
-    }
-  },
-  // 컴포넌트가 생성되기 직전에 실행 됨 (데이터 정의 전)
-  beforeCreate() {
-    console.log("Before Create!!", this.count)
-  },
-  // 컴포넌트가 생성된 직후에 실행 됨
-  created() {
-    console.log("Created!!", this.count)
-    // 컴포넌트가 생성된 직후기 때문에 html 연결되어 있지 않아서 조회 불가능
-    console.log(document.querySelector('h1'))
-  },
-  // html과 연결되기 전이기 때문에 html 구조 조회 불가능
-  beforeMount() {
-    console.log("Before Mount!!")
-    console.log(document.querySelector('h1'))
-  },
-  // 실제 컴포넌트가 html 구조에 연결되어져 있음
-  // 따라서 html 구조를 조회해서 출력 가능
-  mounted() {
-    console.log("Mounted!!")
-    console.log(document.querySelector('h1'))
+  components: {
+    Fruits
   }
 }
 </script>
---
+++ fastCampus/vue3-start/src/components/Fruits.vue
@@ -0,0 +1,49 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <section v-if="hasFruit">
+    <h1>Fruits</h1>
+    <ul>
+      <li
+        v-for="fruit in fruits"
+        :key="fruit">
+        {{ fruit }}
+      </li>
+    </ul>
+  </section>
+
+  <section>
+    <h1>Reverse Fruits</h1>
+    <ul>
+      <li
+        v-for="fruit in reverseFruits"
+        :key="fruit">
+        {{ fruit }}
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fruits: [
+        'Apple', 'Lemon', 'Grape'
+      ]
+    }
+  },
+  computed: {
+    hasFruit() {
+      return this.fruits.length > 0
+    },
+    reverseFruits() {
+      return this.fruits.map(fruit => {
+        // 'Apple' => ['A','p','p','l','e'] 
+        // => ['e','l','p','p','A']
+        // =>'elppA'
+        return fruit.split('').reverse().join('')
+      })
+    }
+  }
+}
+</script>
