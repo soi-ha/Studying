14450523c29f1a8a6674d0fe26eeada4281e46d1
soi-ha <soy2302ten@gmail.com>
2023-01-31 17:04:46 +0900

    클래스와 스타일 바인딩

--- fastCampus/vue3-start/src/App.vue
+++ fastCampus/vue3-start/src/App.vue
@@ -1,33 +1,27 @@
 <template>
-  <h1 @click="changeMessage">
-    {{ msg }}
+  <h1
+    :style="[fontStyle, backgroundStyle]"
+    @click="changeStyle">
+    Hello?!
   </h1>
-  <h1>{{ reversedMessage }}</h1>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      msg: 'Hello?'
-    }
-  },
-  computed: {
-    reversedMessage() {
-      return this.msg.split('').reverse().join('') 
-    }
-  },
-  watch: {
-    msg(newValue) {
-      console.log('msg:', newValue)
-    },
-    reversedMessage() {
-      console.log('reversedMessage:', this.reversedMessage)
+      fontStyle: {
+        color: 'orange',
+        fontSize: '30px'
+      },
+      backgroundStyle: {
+        backgroundStyle: 'black'
+      }
     }
   },
   methods: {
-    changeMessage() {
-      this.msg = 'Good!'
+    changeStyle() {
+      this.fontStyle.color = 'red'
     }
   }
 }
