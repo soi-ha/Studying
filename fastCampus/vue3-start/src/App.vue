2665316700e0a82e54992a12b5b4ad7248dc9982
soi-ha <soy2302ten@gmail.com>
2023-02-01 23:43:50 +0900

    이벤트 핸들링

--- fastCampus/vue3-start/src/App.vue
+++ fastCampus/vue3-start/src/App.vue
@@ -1,41 +1,14 @@
 <template>
-  <button @click="handler">
-    Click me!
-  </button>
-  <ul>
-    <li
-      v-for="fruit in newFruits"
-      :key="fruit.id">
-      {{ fruit.name }}-{{ fruit.id }}
-    </li>
-  </ul>
+  <input
+    type="text"
+    @keydown.a="handler" />
 </template>
 
 <script>
-import shortid from 'shortid'
-
 export default {
-  data() {
-    return {
-      fruits: ['Apple', 'Banana', 'Cherry'],
-      // newFruits: [
-      //   { id: 0, name: 'Apple' },
-      //   { id: 1, name: 'Banana' },
-      //   { id: 2, name: 'Cherry' }
-      // ]
-    }
-  },
-  computed: {
-    newFruits() {
-      return this.fruits.map(fruit => ({
-        id: shortid.generate(),
-        name: fruit
-      }))
-    }
-  },
   methods: {
     handler() {
-      this.fruits.push('Orange')
+      console.log('Enter!!')
     }
   }
 }
