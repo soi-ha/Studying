1c738a2907466d1dfdc465ac751bc6449250ae44
soi-ha <soy2302ten@gmail.com>
2023-02-11 21:52:53 +0900

    Movie: header Logo/ style 수정, a태그 밑줄 밑 hover시 색상 변경

--- fastCampus/vue3-movie-app/src/components/Header.vue
+++ fastCampus/vue3-movie-app/src/components/Header.vue
@@ -43,3 +43,15 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  header {
+    height: 70px;
+    padding: 0 40px;
+    display: flex;
+    align-items: center;
+    .logo {
+      margin-right: 40px;
+    }
+  }
+</style>
--- fastCampus/vue3-movie-app/src/components/Logo.vue
+++ fastCampus/vue3-movie-app/src/components/Logo.vue
@@ -12,6 +12,10 @@
     font-family: "Oswald", sans-serif;
     font-size: 20px;
     color: $black;
+    text-decoration: none;
+    &:hover {
+      color: $black;
+    }
     span {
       color: $primary;
     }
