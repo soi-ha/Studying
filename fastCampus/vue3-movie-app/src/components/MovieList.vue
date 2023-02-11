9f8e002bd662b37ff1c12b12d49ecec2ae9512f6
soi-ha <soy2302ten@gmail.com>
2023-02-12 00:18:16 +0900

    Movie: MovieItem, MoiveList 컴포넌트 생성

---
+++ fastCampus/vue3-movie-app/src/components/MovieList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="inner">
+      <MovieItem
+        v-for="movie in moives"
+        :key="movie.imdbID" />
+    </div>
+  </div>
+</template>
+
+<script>
+import MovieItem from '~/components/MovieItem'
+export default {
+  components: {
+    MovieItem
+  },
+  data() {
+    return {
+      movies: []
+    }
+  }
+}
+</script>
