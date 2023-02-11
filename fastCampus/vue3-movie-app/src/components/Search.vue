86d12b2da31230377702c3048a4fa995870707d5
soi-ha <soy2302ten@gmail.com>
2023-02-11 22:29:02 +0900

    Movie: Search 생성

---
+++ fastCampus/vue3-movie-app/src/components/Search.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <input
+      v-model="title"
+      type="text" 
+      placeholder="Search for Movies, Series $ more" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: ''
+    }
+  }
+}
+</script>
--- fastCampus/vue3-movie-app/src/routes/Home.vue
+++ fastCampus/vue3-movie-app/src/routes/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <Headline />
+  <Search />
 </template>
 
 <script>
 import Headline from "~/components/Headline"
+import Search from "~/components/Search"
 
 export default {
   components: {
-    Headline
+    Headline,
+    Search
   }
 }
 </script>
