45cccb7d7d21c2041fe4690a4ecef5d01635ac50
soi-ha <soy2302ten@gmail.com>
2023-02-11 23:16:31 +0900

    Movie: Search 필터 데이터 생성 및 연결

--- fastCampus/vue3-movie-app/src/components/Search.vue
+++ fastCampus/vue3-movie-app/src/components/Search.vue
@@ -2,8 +2,27 @@
   <div class="container">
     <input
       v-model="title"
+      class="form-control"
       type="text" 
       placeholder="Search for Movies, Series $ more" />
+    <div class="selects">
+      <select
+        v-for="filter in filters"
+        v-model="$data[filter.name]"
+        :key="filter.name"
+        class="form-select">
+        <option
+          v-if="filter.name == 'year'"
+          value="">
+          All Years
+        </option>
+        <option
+          v-for="item in filter.items"
+          :key="item">
+          {{ item }}
+        </option>
+      </select>
+    </div>
   </div>
 </template>
 
@@ -11,8 +30,55 @@
 export default {
   data() {
     return {
-      title: ''
+      title: '',
+      type: 'movie',
+      number: '10',
+      year: '',
+      filters: [
+        {
+          name: 'type',
+          items: ['movie', 'series','episode']
+        },
+        {
+          name: 'number',
+          items: [10,20,30]
+        },
+        {
+          name: 'year',
+          items: (() => {
+            const years = []
+            const thisYear = new Date().getFullYear()
+            for (let i = thisYear; i >= 1985; i -= 1) {
+              years.push(i)
+            }
+            return years
+          })()
+        },
+      ]
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .container {
+    display: flex;
+    > * { // .container의 모든 자식요소를 의미
+      margin-right: 10px;
+      font-size: 15px;
+      &:last-child { // 자식요소들 중의 마지막 자식요소
+        margin-right: 0;
+      }
+    }
+    .selects {
+      display: flex;
+      select {
+        width: 120px;
+        margin-right: 10px;
+        &:last-child {
+          margin-right: 0;
+        }
+      }
+    }
+  }
+</style>
