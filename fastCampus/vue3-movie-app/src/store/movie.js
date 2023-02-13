4638292328d60cf18832562fb2c42b084de5492a
soi-ha <soy2302ten@gmail.com>
2023-02-13 23:59:48 +0900

    Movie: 영화검색 추가 요청

--- fastCampus/vue3-movie-app/src/store/movie.js
+++ fastCampus/vue3-movie-app/src/store/movie.js
@@ -20,7 +20,7 @@ export default {
     }
   },
   actions: {
-    async searchMovies({ commit }, payload) {
+    async searchMovies({ state, commit }, payload) {
       const { title, type, number, year } = payload
       const OMDB_API_KEY = '7035c60c'
       
@@ -29,6 +29,24 @@ export default {
       commit('updateState', {
         movies: Search
       })
+      console.log(totalResults) 
+      console.log(typeof totalResults)
+
+      const total = parseInt(totalResults, 10)
+      const pageLength = Math.ceil(total / 10)
+
+      // 추가 요청
+      if (pageLength > 1) {
+        for (let page = 2; page <= pageLength; page += 1 ) {
+          // 영화를 최대 30개까지만 가져오도록 if 사용
+          if (page > (number / 10)) break
+          const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)
+          const { Search } = res.data
+          commit('updateState', {
+            movies: [...state.movies, ...Search]
+          })
+        }
+      }
     }
   }
 }
