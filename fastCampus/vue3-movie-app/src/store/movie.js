970fbed47f40aeeaa982d007bd8dce453fde513f
soi-ha <soy2302ten@gmail.com>
2023-02-14 00:16:08 +0900

    Movie: 중복 ID 제거

--- fastCampus/vue3-movie-app/src/store/movie.js
+++ fastCampus/vue3-movie-app/src/store/movie.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import _uniqBy from 'lodash/uniqBy'
 
 export default {
   namespaced: true,
@@ -27,7 +28,7 @@ export default {
       const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
       const { Search, totalResults } = res.data
       commit('updateState', {
-        movies: Search
+        movies: _uniqBy(Search, 'imdbID')
       })
       console.log(totalResults) 
       console.log(typeof totalResults)
@@ -43,7 +44,9 @@ export default {
           const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)
           const { Search } = res.data
           commit('updateState', {
-            movies: [...state.movies, ...Search]
+            movies: [
+              ...state.movies, 
+              ..._uniqBy(Search, 'imdbID')]
           })
         }
       }
