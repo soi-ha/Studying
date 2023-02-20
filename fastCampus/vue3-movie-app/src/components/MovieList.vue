d03be18316132bdd9e7b1894590132bf95b73678
soi-ha <soy2302ten@gmail.com>
2023-02-20 16:03:32 +0900

    Movie: _fetchMovies 작성, error 발생시 error 메세지 출력되도록 작성

--- fastCampus/vue3-movie-app/src/components/MovieList.vue
+++ fastCampus/vue3-movie-app/src/components/MovieList.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container">
     <div class="inner">
+      <div class="message">
+        {{ message }}
+      </div>
       <MovieItem
         v-for="movie in movies"
         :key="movie.imdbID" 
@@ -20,6 +23,9 @@ export default {
       return this.$store.state.movie.movies
       // store에 접근해서 스토어의 state(상태)중에 
       // movie 모듈에 접근하여 moives 데이터를 가져와서 moives라는 계산된 메소드에 사용한다.
+    },
+    message() {
+      return this.$store.state.movie.message
     }
   }
 }
--- fastCampus/vue3-movie-app/src/store/movie.js
+++ fastCampus/vue3-movie-app/src/store/movie.js
@@ -22,34 +22,65 @@ export default {
   },
   actions: {
     async searchMovies({ state, commit }, payload) {
-      const { title, type, number, year } = payload
-      const OMDB_API_KEY = '7035c60c'
-      
-      const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
-      const { Search, totalResults } = res.data
-      commit('updateState', {
-        movies: _uniqBy(Search, 'imdbID')
-      })
-      console.log(totalResults) 
-      console.log(typeof totalResults)
-
-      const total = parseInt(totalResults, 10)
-      const pageLength = Math.ceil(total / 10)
-
-      // 추가 요청
-      if (pageLength > 1) {
-        for (let page = 2; page <= pageLength; page += 1 ) {
-          // 영화를 최대 30개까지만 가져오도록 if 사용
-          if (page > (number / 10)) break
-          const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)
-          const { Search } = res.data
-          commit('updateState', {
-            movies: [
-              ...state.movies, 
-              ..._uniqBy(Search, 'imdbID')]
-          })
+      try {
+        const res = await _fetchMovie({
+          ...payload,
+          page: 1
+        })
+        const { Search, totalResults } = res.data
+        commit('updateState', {
+          movies: _uniqBy(Search, 'imdbID')
+        })
+        console.log(totalResults) 
+        console.log(typeof totalResults)
+  
+        const total = parseInt(totalResults, 10)
+        const pageLength = Math.ceil(total / 10)
+  
+        // 추가 요청
+        if (pageLength > 1) {
+          for (let page = 2; page <= pageLength; page += 1 ) {
+            // 영화를 최대 30개까지만 가져오도록 if 사용
+            if (page > (payload.number / 10)) break
+            const res = await _fetchMovie({
+              ...payload,
+              page
+            })
+            const { Search } = res.data
+            commit('updateState', {
+              movies: [
+                ...state.movies, 
+                ..._uniqBy(Search, 'imdbID')]
+            })
+          }
         }
+      } catch (message) {
+        commit('updateState', {
+          movies: [],
+          message
+        })
       }
     }
   }
+}
+
+// 해당 내용이 복잡해질수록 재활욜성이 높아진다.
+// 안에 내용들을 반복적으로 작성할 필요가 없다는 이야기니까.
+function _fetchMovie(payload) {
+  const { title, type, year, page } = payload
+  const OMDB_API_KEY = '7035c60c'
+  const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
+
+  return new Promise((resolve, reject) => {
+    axios.get(url)
+      .then(res => {
+        if (res.data.Error) {
+          reject(res.data.Error)
+        }
+        resolve(res)
+      })
+      .catch(err => {
+        reject(err.message)
+      })
+  })
 }
