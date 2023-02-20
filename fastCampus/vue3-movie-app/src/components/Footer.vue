296b44aba5cb07250e85660c67aa7bac03cca914
soi-ha <soy2302ten@gmail.com>
2023-02-20 21:44:06 +0900

    Movie: Footer 생성

---
+++ fastCampus/vue3-movie-app/src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer>
+    <Logo />
+    <a
+      href="https://github.com/soi-ha"
+      target="_blank">
+      (c){{ new Date().getFullYear() }} SOHA
+    </a>
+  </footer>
+</template>
+
+<script>
+import Logo from '~/components/Logo'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+footer {
+  padding: 70px 0;
+  text-align: center;
+  opacity: .3;
+  .logo {
+    display: black;
+    margin-bottom: 4px;
+  }
+}
+</style>
