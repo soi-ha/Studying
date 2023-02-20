2f86f871763b9f32ced3b1b72a028326df28dd16
soi-ha <soy2302ten@gmail.com>
2023-02-20 21:03:20 +0900

    Movie: container 너비 사용자 지정

--- fastCampus/vue3-movie-app/src/components/MovieList.vue
+++ fastCampus/vue3-movie-app/src/components/MovieList.vue
@@ -34,7 +34,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~/scss/main";
+
 .container {
+  margin-top: 30px;
+  .inner {
+    background-color: $gray-200;
+    padding: 10px;
+    border-radius: 4px;
+  }
   .movies {
     display: flex;
     flex-wrap: wrap;
