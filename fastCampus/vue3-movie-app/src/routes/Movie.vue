c2d9593d616fb68c2c51ae510ddde015287e30b4
soi-ha <soy2302ten@gmail.com>
2023-02-28 14:48:22 +0900

    Movie: skelton UI

--- fastCampus/vue3-movie-app/src/routes/Movie.vue
+++ fastCampus/vue3-movie-app/src/routes/Movie.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="container">
     <div class="skeletons">
+      <div class="skeleton poster"></div>
+      <div class="specs">
+        <div class="skeleton title"></div>
+        <div class="skeleton spec"></div>
+        <div class="skeleton plot"></div>
+        <div class="skeleton etc"></div>
+        <div class="skeleton etc"></div>
+        <div class="skeleton etc"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -15,3 +24,45 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import '~/scss/main';
+.container {
+  padding-top: 40px;
+}
+.skeletons {
+  display: flex;
+  .poster {
+    flex-shrink: 0;
+    width: 500px;
+    height: 500px * 3 / 2;
+    margin-right: 70px;
+  }
+  .specs {
+    flex-grow: 1;
+  }
+  .skeleton {
+    border-radius: 10px;
+    background-color: $gray-200;
+    &.title {
+      width: 80%;
+      height: 70px;
+    }
+    &.spec {
+      width: 60%;
+      height: 30px;
+      margin-top: 20px;
+    }
+    &.plot {
+      width: 100%;
+      height: 250px;
+      margin-top: 20px;
+    }
+    &.etc {
+      width: 50%;
+      height: 50px;
+      margin-top: 20px;
+    }
+  }
+}
+</style>
