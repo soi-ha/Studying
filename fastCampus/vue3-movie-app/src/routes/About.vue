956e78eca8c5d40528a8403f679ce8a978377f67
soi-ha <soy2302ten@gmail.com>
2023-03-01 23:04:43 +0900

    Movie: About 페이지

--- fastCampus/vue3-movie-app/src/routes/About.vue
+++ fastCampus/vue3-movie-app/src/routes/About.vue
@@ -1,3 +1,85 @@
 <template>
-  <h1>About!</h1>
+  <div class="about">
+    <div class="photo">
+      <Loader
+        v-if="imageLoading"
+        absolute />
+      <img 
+        :src="image" 
+        :alt="name" />
+    </div>
+    <div class="name">
+      {{ name }}
+    </div>
+    <div>{{ email }}</div>
+    <div>{{ blog }}</div>
+    <div>{{ phone }}</div>
+  </div>
 </template>
+
+<script>
+import Loader from '~/components/Loader'
+export default {
+  components: {
+    Loader
+  },
+  data() {
+    return {
+      imageLoading: true
+    }
+  },
+  computed: {
+    image() {
+      return this.$store.state.about.image
+    },
+    name() {
+      return this.$store.state.about.name
+    },
+    email() {
+      return this.$store.state.about.email
+    },
+    blog() {
+      return this.$store.state.about.blog
+    },
+    phone() {
+      return this.$store.state.about.phone
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    async init() {
+      await this.$loadImage(this.image)
+      this.imageLoading = false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scpoed>
+@import "~/scss/main";
+
+.about {
+  text-align: center;
+  .photo {
+    width: 250px;
+    height: 250px;
+    margin: 40px auto 20px;
+    padding: 30px;
+    border: 10px solid $gray-300;
+    border-radius: 50%;
+    box-sizing: border-box;
+    background-color: $gray-200;
+    position: relative;
+    img {
+      width: 100%;
+    }
+  }
+  .name {
+    font-size: 40px;
+    font-family: "Oswald", sans-serif;
+    margin-bottom: 20px;
+  }
+}
+</style>
---
+++ fastCampus/vue3-movie-app/src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'SOHA',
+    email: 'soy2302ten@gmail.com',
+    blog: 'https://soi-ha.github.io/',
+    phone: '+82-10-1234-5678',
+    image: 'https://heropy.blog/css/images/logo.png'
+  })
+}
