0fe36ba06155ce5b585c6d4ca01328faacc9e0f0
soi-ha <soy2302ten@gmail.com>
2023-03-01 23:24:33 +0900

    Movie: 404 page not found

--- fastCampus/vue3-movie-app/src/routes/index.js
+++ fastCampus/vue3-movie-app/src/routes/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from './Home'
 import Movie from './Movie'
 import About from './About'
+import NotFound from './NotFound'
 
 export default createRouter ({
   // Hash
@@ -21,6 +22,10 @@ export default createRouter ({
     {
       path: '/about',
       component: About
+    },
+    {
+      path: '/:notFound(.*)',
+      component: NotFound
     }
   ]
 })
