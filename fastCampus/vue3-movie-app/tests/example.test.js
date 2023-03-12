2e42d02857e47781aa7dd7eda34f3895cef2449c
soi-ha <soy2302ten@gmail.com>
2023-03-13 00:36:36 +0900

    jest globals

--- fastCampus/vue3-movie-app/tests/example.test.js
+++ fastCampus/vue3-movie-app/tests/example.test.js
@@ -1,14 +1,33 @@
 import { double } from './example'
 
-test('첫 테스트', () => {
-  expect(123).toBe(123)
-})
+describe('Group1', () => {
+  beforeAll(() => {
+    console.log('beforeAll!')
+  })
+  afterAll(() => {
+    console.log('afterAll!')
+  })
 
-test('인수가 숫자 데이터입니다', () => {
-  expect(double(3)).toBe(6)
-  expect(double(10)).toBe(20)
-})
+  beforeEach(() => {
+    console.log('beforeEach!')
+  })
+  afterEach(() => {
+    console.log('afterEach!')
+  })
 
-test('인수가 없습니다', () => {
-  expect(double()).toBe(0)
+  test('첫 테스트', () => {
+    console.log('첫 테스트!')
+    expect(123).toBe(123)
+  })
+  
+  test('인수가 숫자 데이터입니다', () => {
+    console.log('인수가 숫자 데이터입니다!')
+    expect(double(3)).toBe(6)
+    expect(double(10)).toBe(20)
+  })
+  
+  test('인수가 없습니다', () => {
+    console.log('인수가 없습니다!')
+    expect(double()).toBe(0)
+  })
 })
