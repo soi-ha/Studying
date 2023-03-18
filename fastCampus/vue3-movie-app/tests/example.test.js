42c49bbbe3eea2f7fbaad3b3b7a84532be9a8be6
soi-ha <soy2302ten@gmail.com>
2023-03-18 17:07:58 +0900

    VTU 테스트, wrapper 객체

--- fastCampus/vue3-movie-app/tests/example.test.js
+++ fastCampus/vue3-movie-app/tests/example.test.js
@@ -1,18 +1,14 @@
-import axios from 'axios';
-import { fetchMovieTitle } from './example';
+import { mount } from '@vue/test-utils';
+import Example from './Example.vue';
 
-describe('비동기 테스트', () => {
-  test('영화 제목 반환', async () => {
-    axios.get = jest.fn(() => {
-      return new Promise(resolve => {
-        resolve({
-          data: {
-            Title: 'Frozen II'
-          }
-        })
-      })
-    })
-    const title = await fetchMovieTitle()
-    expect(title).toBe('Frozen ii')
+test('메세지를 변경합니다',async () => {
+  const wrapper = mount(Example)
+  // wrapper.vm == this
+  expect(wrapper.vm.msg).toBe('Hello Vue Test Utils!')
+  // wrapper.vm.msg = 'Hello Soha!'
+  await wrapper.setData({
+    msg: 'Hello Soha!'
   })
-})
+  expect(wrapper.vm.msg).toBe('Hello Soha!')
+  expect(wrapper.find('div').text()).toBe('Hello Soha!')
+} )
