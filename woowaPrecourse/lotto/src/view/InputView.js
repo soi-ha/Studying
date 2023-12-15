62d89220340685bb2f66d695bafd211633265ecb
soi-ha <soy2302ten@gmail.com>
2023-12-15 21:13:53 +0900

    feat: View 기능 구현

--- woowaPrecourse/lotto/src/view/InputView.js
+++ woowaPrecourse/lotto/src/view/InputView.js
@@ -1,9 +1,17 @@
 import { Console } from '@woowacourse/mission-utils';
 
 const InputView = {
-  async writeCar() {
-    const input = await Console.readLineAsync('내용\n');
-    return input;
+  async writeMoney() {
+    const input = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+    return Number(input);
+  },
+  async writeWinnerNumber() {
+    const input = await Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
+    return input.split(',');
+  },
+  async writeBonusNumber() {
+    const input = await Console.readLineAsync('보너스 번호를 입력해 주세요.\n');
+    return Number(input);
   },
 };
 
--- woowaPrecourse/lotto/src/view/OutputView.js
+++ woowaPrecourse/lotto/src/view/OutputView.js
@@ -1,5 +1,21 @@
 import { Console } from '@woowacourse/mission-utils';
 
-const OutputView = {};
+const OutputView = {
+  printLotto() {
+    Console.print('{}개를 구매했습니다.');
+    Console.print('[]');
+  },
+
+  printWinningStatistics() {
+    Console.print(`당첨 통계\n
+    ---\n
+    3개 일치 (5,000원) - {}개\n
+    4개 일치 (50,000원) - 0{}개\n
+    5개 일치 (1,500,000원) - {}개\n
+    5개 일치, 보너스 볼 일치 (30,000,000원) - {}개\n
+    6개 일치 (2,000,000,000원) - {}개\n
+    총 수익률은 {}%입니다.`);
+  },
+};
 
 export default OutputView;
